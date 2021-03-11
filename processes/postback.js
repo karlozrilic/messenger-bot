9e9a1acde775b23639736416e5d4d81a428b7e7b
karlozrilic <kazrile@gmail.com>
2021-03-11 12:53:14 +0100

    fixes

--- processes/postback.js
+++ processes/postback.js
@@ -27,7 +27,7 @@ const sendMessage = require('../templates/sendMessage');
             let message = greeting + "Welcome to Healthbot. Hope you are doing good today";
             let message2 = "I am your nutrition tracker :-)"
             let message3 = "please type in what you ate like: I ate chicken birayani and 2 chapatis with dal.";
-            senderAction(senderID);
+            /*senderAction(senderID);*/
             sendMessage(senderID, {text: message}).then(() => {
                 sendMessage(senderID, { text: message2 }).then(() => {
                     sendMessage(senderID, {  text: message3}).then(() => {
