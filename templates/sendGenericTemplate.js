d733c824078adb4db49b74bcdc5b7ffc8b6cd7d5
karlozrilic <kazrile@gmail.com>
2021-03-11 10:42:17 +0100

    fixes

--- templates/sendGenericTemplate.js
+++ templates/sendGenericTemplate.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const messages = require('../processes/messages');
 
 module.exports = function sendGenericTemplate(recipientId, respBody) {
     console.log(respBody);
@@ -24,12 +25,18 @@ module.exports = function sendGenericTemplate(recipientId, respBody) {
     }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-            method: 'POST',
-            json: {
-                recipient: {id: recipientId},
-                message: messageData,
-            }
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            "text": "hello"
+        }
+        /*qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: messageData,
+        }*/
     }, function(error, response, body){
         if (error) {
             console.log("Error sending message: " + response.error)
