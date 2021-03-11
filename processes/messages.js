b51ab0df7c5b03e1b54e3d21eab24e2c82281b6b
karlozrilic <kazrile@gmail.com>
2021-03-11 12:05:06 +0100

    fixes

--- processes/messages.js
+++ processes/messages.js
@@ -5,8 +5,6 @@ module.exports = function processMessage(event) {
     if (!event.message.is_echo) {
         const message = event.message;
         const senderID = event.sender.id;
-        console.log("Received message from senderId: " + senderID);
-        console.log("Message is: " + JSON.stringify(message));
         if (message.text) {
             let text = message.text;
             senderAction(senderID, text);
