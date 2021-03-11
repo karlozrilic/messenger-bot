439ac4891640f2cdc19f489ae7374aaa4da3b584
karlozrilic <kazrile@gmail.com>
2021-03-11 14:14:50 +0100

    fixes

--- templates/senderAction.js
+++ templates/senderAction.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const sendMessage = require('./sendMessage');
 
 module.exports = function senderAction(recipientId, messageText) {
 
@@ -38,19 +39,5 @@ module.exports = function senderAction(recipientId, messageText) {
         };
     }
 
-    request({
-        url: "https://graph.facebook.com/v2.6/me/messages",
-        qs: {
-            access_token: process.env.PAGE_ACCESS_TOKEN
-        },
-        method: "POST",
-        json: {
-            recipient: {id: recipientId},
-            message: message
-        }
-    }, function(error, response) {
-        if (error) {
-            console.log("Error sending message: " + response.error);
-        }
-    });
+    sendMessage(message);
 }
