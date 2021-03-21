1b88c26e8bffeb82ca495d7dfdf11c5c6eae66db
karlozrilic <kazrile@gmail.com>
2021-03-21 16:06:33 +0100

    fixes

--- templates/senderAction.js
+++ templates/senderAction.js
@@ -39,39 +39,8 @@ module.exports = function senderAction(recipientId, messageText, event) {
         }
         sendMessage(recipientId, message);
     } else if (messageText == "i want in!") {
-        const senderID = event.sender.id;
-        request({
-            url: "https://graph.facebook.com/v2.6/" + senderID,
-            qs: {
-                access_token: process.env.PAGE_ACCESS_TOKEN,
-                fields: "first_name"
-            },
-            method: "GET"
-        }, function(error, response, body) {
-            let greeting = '';
-            if (error) {
-                console.error("Error getting user name: " + error);
-            } else {
-                let bodyObject = JSON.parse(body);
-                console.log(bodyObject);
-                name = bodyObject.first_name;
-                greeting = "Hello " + name  + ". ";
-            }
-            let message = greeting + "Welcome to Digital nomads - Zadar. Hope you are doing good today";
-            let message2 = "I am here to help you :-)"
-            let message3 = `please type in one of the following commands:
-                            - color
-                            - hi`;
-            /*senderAction(senderID);*/
-            sendMessage(senderID, {text: message}).then(() => {
-                sendMessage(senderID, { text: message2 }).then(() => {
-                    sendMessage(senderID, {  text: message3}).then(() => {
-                        sendMessage(senderID, { text: '🎈' });
-                    })
-                });
-            });
-        });
-        //processPostback(event);
+        
+        processPostback(event);
     } else {
         odg = "I don't understand";
         message = {
