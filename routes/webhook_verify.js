517137b54aa2fdea74aaea2bf9f63925a1b97b91
karlozrilic <kazrile@gmail.com>
2021-06-24 22:37:20 +0200

    fix

--- routes/webhook_verify.js
+++ routes/webhook_verify.js
@@ -44,6 +44,7 @@ export const verifyWebhook = (app, chalk) => {
    	});
 
 	app.get('/webhook', function(req, res) {
+		console.log(process.env.VERIFY_TOKEN)
 		if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
 			console.log('webhook verified');
 			res.status(200).send(req.query['hub.challenge']);
