5cb9959c4be7fcb0ff703b18f1da16240030c5c9
karlozrilic <kazrile@gmail.com>
2021-06-25 19:14:00 +0200

    fix

--- responses/responses.js
+++ responses/responses.js
@@ -2,16 +2,16 @@ export default
 {
     "greetings": {
         "en": {
-            "messages": ["Hello $. Welcome to Digital nomads - Zadar. Hope you are doing good today.", "I am here to help you :-)", "Please type in one of the following commands: $"]
+            "messages": ["Hello $. Welcome to Digital nomads - Zadar. Hope you are doing good today.", "I am here to help you :-)", "Please type in one of the following commands: $\n\nIf you wish to see description of each command type ' -d' after."]
         },
         "es": {
-            "messages": ["Hola $. Bienvenido a Digital nomads - Zadar. Espero que estés bien hoy.", "Estoy aqui para ayudarte :-)", "Escriba uno de los siguientes comandos: $"]
+            "messages": ["Hola $. Bienvenido a Digital nomads - Zadar. Espero que estés bien hoy.", "Estoy aqui para ayudarte :-)", "Escriba uno de los siguientes comandos: $\n\nSi desea ver la descripción de cada comando, escriba ' -d' después"]
         },
         "de": {
-            "messages": ["Hallo $. Willkommen bei Digital Nomads - Zadar. Ich hoffe, es geht dir heute gut.", "Ich bin hier, um dir zu helfen :-)", "Bitte geben Sie einen der folgenden Befehle ein: $"]
+            "messages": ["Hallo $. Willkommen bei Digital Nomads - Zadar. Ich hoffe, es geht dir heute gut.", "Ich bin hier, um dir zu helfen :-)", "Bitte geben Sie einen der folgenden Befehle ein: $\n\nWenn Sie die Beschreibung jedes Befehls sehen möchten, geben Sie ' -d' nach ein."]
         },
         "fr": {
-            "messages": ["Bonjour $. Bienvenue chez Digital nomads - Zadar. J'espère que tu vas bien aujourd'hu.", "Je suis ici pour vous aider :-)", "Veuillez saisir l'une des commandes suivantes: $"]
+            "messages": ["Bonjour $. Bienvenue chez Digital nomads - Zadar. J'espère que tu vas bien aujourd'hu.", "Je suis ici pour vous aider :-)", "Veuillez saisir l'une des commandes suivantes: $\n\nSi vous souhaitez voir la description de chaque commande tapez ' -d' après"]
         }
     },
     "languages": {
